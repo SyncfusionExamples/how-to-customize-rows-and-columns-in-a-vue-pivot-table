cf795cce15c39076505e5e66beced7686034d7a0
MeenaAlagiah <117711147+MeenaAlagiah@users.noreply.github.com>
2023-07-18 17:56:44 +0530

    Committing the Vue Pivot Table Row and Column sample

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import {registerLicense} from '@syncfusion/ej2-base';
+registerLicense("ORg4AjUWIQA/Gnt2V1hhQlJCfVhdXGpWfFN0RnNedVx2flRCcC0sT3RfQF5jTn5UdkJhXnpWcH1cQA==");
+createApp(App).mount('#app')
